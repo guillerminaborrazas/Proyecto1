0c8627a503527fc95795f353a9f8cad8648eb605
Maria Guillermina Borrazas <guillermina.borrazas@gmail.com>
2023-06-15 12:00:17 -0300

    Primer parte playground coder

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+def inicio(request):
+    return(render(request, "AppCoder/inicio.html"))
+
+def cursos(request):
+    return(render(request, "AppCoder/cursos.html"))
+
+def profesores(request):
+    return(render(request, "AppCoder/profesores.html"))
+
+def estudiantes(request):
+    return("Vista estudiante")
+
+def entregables(request):
+    return("Vista entregables")
